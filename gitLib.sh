a8871dd6d385857142fd22812adaf4cbd1043336
Valmira Nogueira Corrêa <vnogue@br.ibm.com>
2021-03-25 11:13:38 -0300

    Correct clone

--- gitLib.sh
+++ gitLib.sh
@@ -3,12 +3,11 @@
 function clonaRepo()
 {
   # Checking function arguments
-  if test $# -eq 0 ; then
-    echo "No arguments supplied"
-  elif test -z "$1" -o -z "$2" -o -z "$3" ; then
+  if test -z "$1" -o -z "$2" -o -z "$3" ; then
     echo "Missing required arguments"
   else
-    gitrepo=$(echo $1 | sed -e "s/github.com/$2@github.com/g")
+    # Running clone
+    gitrepo="https://github.com/$2/$1.git"
     git clone $gitrepo --branch $3
   fi
 }
